a5e609d1ed0977e6b9b17bcafc67242337d3cddb
unknown <mbeales@spartaglobal.com>
2018-12-13 21:49:04 +0000

    Created x when clicking on box

--- script.js
+++ script.js
@@ -5,21 +5,29 @@ document.addEventListener('DOMContentLoaded', function() {
       var box = document.getElementsByTagName("td");
       // create the x.
 
-      // Creat a click event add an x to a box when it is clicked
+      // // Create a click event add an x to a box when it is clicked
       for (var i = 0; i < box.length; i++) {
-        box[i].addEventListener("click", xClick)
+        box[i].addEventListener("click", xclick);
       }
-      // Need to check that box are empty. If so, add an x to clicked square.
-      function xClick () {
-        console.log(box)
+      // // Need to check that box are empty. If so, add an x to clicked square.
+      function xclick() {
+        for (var i = 0; i < box.length; i++) {
+          this.classList.add("X");
+          this.innerHTML = "X";
+        }
       }
+
+
 
     // Need to add a o to one of the squares.
       // Change text to say it is o turn
-      // Creat a click event add an x to a box when it is clicked. (already done)
+      // Creat a click event add an x to a box when it is clicked.
+      // for (var i = 0; i < box.length; i++) {
+      //   box[i].addEventListener("click", yClick)
+      // }
       // Need to check if there is already something in the squares
         // Need to check that box are empty. If so, add an o to clicked square. if square if full, don't allow o to be added
-        function
+
     //Do the same process as above for x. Keep doing this.
 
     // Apply win conditions
