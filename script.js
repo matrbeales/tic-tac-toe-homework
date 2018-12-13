85e25afffdba7030dd156520b070aeac68e71538
unknown <mbeales@spartaglobal.com>
2018-12-13 22:10:00 +0000

    allowed x and o to be added in turns

--- script.js
+++ script.js
@@ -6,28 +6,39 @@ document.addEventListener('DOMContentLoaded', function() {
       // create the x.
 
       // // Create a click event add an x to a box when it is clicked
-      for (var i = 0; i < box.length; i++) {
+      for (var i = 0; i < box.length; i+=2) {
         box[i].addEventListener("click", xclick);
       }
       // // Need to check that box are empty. If so, add an x to clicked square.
-      function xclick() {
-        for (var i = 0; i < box.length; i++) {
-          this.classList.add("X");
-          this.innerHTML = "X";
+
+        function xclick() {
+          for (var i = 0; i < box.length; i++) {
+            if (this.Classlist != "O") {
+              this.classList.add("X");
+              this.innerHTML = "X";
+            }
+          }
         }
-      }
 
 
 
     // Need to add a o to one of the squares.
       // Change text to say it is o turn
+
       // Creat a click event add an x to a box when it is clicked.
-      // for (var i = 0; i < box.length; i++) {
-      //   box[i].addEventListener("click", yClick)
-      // }
+      for (var i = 1; i < box.length; i+=2) {
+        box[i].addEventListener("click", oclick);
+      }
       // Need to check if there is already something in the squares
         // Need to check that box are empty. If so, add an o to clicked square. if square if full, don't allow o to be added
-
+        function oclick() {
+          for (var i = 0; i < box.length; i++) {
+            if (this.Classlist != "X") {
+              this.classList.add("O");
+              this.innerHTML = "O";
+            }
+          }
+        }
     //Do the same process as above for x. Keep doing this.
 
     // Apply win conditions
